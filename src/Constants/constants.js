bd8a29003d502c1363bdec37e979662d7e8cc91c
codewithrabeeh <real.ray48@gmail.com>
2022-09-01 13:37:01 +0530

    unpurged

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,6 +1,6 @@
 
 export const baseUrl = 'https://api.themoviedb.org/3'
-export const API_KEY = api_key
+export const API_KEY = '382ed2b378c7475a00eabb7906d18e69'
 export const imageUrl = 'https://image.tmdb.org/t/p/original'
 
 export const array = [`${baseUrl}/trending/all/week?api_key=${API_KEY}&language=en-US`,
