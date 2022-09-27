c2d002434e87ef08dfc69d7b3bfc870dcd5dc7de
codewithrabeeh <rabeehebrahim48@gmail.com>
2022-09-27 14:48:38 +0530

    added minimum length to description

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -20,7 +20,7 @@ function Banner() {
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'>{movie ? movie.overview : ""}</h1>            
+            <h1 className='description'>{movie ? movie.overview.slice(0,285) + '...' : ""}</h1>            
         </div>
         <div className="fade_bottom"></div>
     </div>
